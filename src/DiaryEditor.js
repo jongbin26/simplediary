f428a64e5a4256d663babab41a4be2d17117a49e
jongbin26 <010jjj@naver.com>
2022-10-04 14:46:25 +0000

    react-clone-coding

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,5 +1,8 @@
-import React, {useState} from 'react'
-const DiaryEditor = () => {
+import React, { useRef, useState } from 'react'
+const DiaryEditor = ({onCreate}) => {
+    
+    const authorInput = useRef();
+    const contentInput = useRef();
     const [state, setState] = useState({
         author : "",
         content : "",
@@ -14,15 +17,30 @@ const DiaryEditor = () => {
     };
     
     const handleSubmit = () => {
-        console.log(state);
+        if(state.author.length < 1){
+            authorInput.current.focus();
+            return;
+        }
+        if(state.content.length < 5){
+            contentInput.current.focus();
+            return;
+        }
+        
+        onCreate(state.author, state.content, state.emotion);
         alert("저장 성공");
-    }
+        setState({
+            author: "",
+            content: "",
+            emtion: 1,
+        });
+    };
     
     return( 
     <div className='DiaryEditor'>
     	<h2>오늘의 일기</h2>
         <div>
             <input
+                ref = {authorInput}
                 name = "author"
                 value = {state.author}
                 onChange = {handleChangeState}
@@ -30,6 +48,7 @@ const DiaryEditor = () => {
         </div>
         <div>
              <textarea
+                 ref = {contentInput}
                  name = "content"
                  value = {state.content}
                  onChange = {handleChangeState}
