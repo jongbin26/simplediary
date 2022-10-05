dcf61117f2de99eda6544565573f77e7fe824d28
jongbin26 <010jjj@naver.com>
2022-10-05 14:47:26 +0000

    react-clone-coding

--- src/App.js
+++ src/App.js
@@ -3,34 +3,9 @@ import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList'
 import './App.css';
 
-// const dummyList = [
-//     {
-//         id:1,
-//         author:"조종빈",
-//         content:"내용1",
-//         emotion:5,
-//         created_date: new Date().getTime()
-//     },
-//     {
-//         id:2,
-//         author:"김준영",
-//         content:"내용2",
-//         emotion:4,
-//         created_date: new Date().getTime()
-//     },
-//     {
-//         id:3,
-//         author:"송규호",
-//         content:"내용3",
-//         emotion:1,
-//         created_date: new Date().getTime()
-//     }
-// ]
-
 function App(){
     const [data, setData] = useState([]);
-    
-    const dataId = useRef(0)
+    const dataId = useRef(1)
     
     const onCreate = (author, content, emotion) =>{
         const created_date = new Date().getTime();
@@ -44,16 +19,22 @@ function App(){
         dataId.current += 1;
         setData([newItem, ...data]);
     };
-    const onDelete = (targetId) => {
+    const onRemove = (targetId) => {
         console.log(`${targetId}가 삭제되었습니다`);
         const newDiaryList = data.filter((it) => it.id !== targetId);
         setData(newDiaryList);
     };
     
+    const onEdit = (targetId, newContent) => {
+        setData(
+            data.map((it) => it.id === targetId ? {...it, content : newContent} : it)
+        );
+    };
+    
     return(
     	<div className="App">
             <DiaryEditor onCreate={onCreate}/>
-            <DiaryList onDelete = {onDelete} diaryList={data}/>
+            <DiaryList onEdit = {onEdit} onRemove = {onRemove} diaryList={data}/>
         </div>
     );
 }
