6f083306b1c96857ba7721b39f4383d5f75e4a9e
jongbin26 <010jjj@naver.com>
2022-10-06 14:15:11 +0000

    react-clone-coding

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import React , {useState, useRef} from 'react';
+import React , {useState, useRef, useEffect} from 'react';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList'
 import './App.css';
 
+//https://jsonplaceholder.typicode.com/comments
+
 function App(){
+    
+    const getData = async() => {
+        const res = await fetch('https://jsonplaceholder.typicode.com/comments').then((res)=>res.json());
+        
+        const initData = res.slice(0, 20).map((it)=>{
+            return {
+                author: it.email,
+                content : it.body,
+                emotion : Math.floor(Math.random() * 5) + 1,
+                created_date : new Date().getTime(),
+                id : dataId.current++
+            }
+        })
+        
+        setData(initData);
+    }; 
+    
+    useEffect(()=>{
+        getData();
+    }, []);
+    
     const [data, setData] = useState([]);
     const dataId = useRef(1)
-    
     const onCreate = (author, content, emotion) =>{
         const created_date = new Date().getTime();
         const newItem = {
